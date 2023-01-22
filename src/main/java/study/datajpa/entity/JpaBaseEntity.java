4af141738e8fabc81c7f0f28ab87cc1191c0daeb
Leo <drd9811@naver.com>
2023-01-22 20:30:10 +0900

    Auditing (등록일, 수정일)

---
+++ src/main/java/study/datajpa/entity/JpaBaseEntity.java
@@ -0,0 +1,29 @@
+package study.datajpa.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+public class JpaBaseEntity {
+
+    @Column(updatable = false)
+    private LocalDateTime createTime;
+    private LocalDateTime updateTime;
+
+    @PrePersist
+    public void prePersist() {
+        this.createTime = LocalDateTime.now();
+        this.updateTime = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updateTime = LocalDateTime.now();
+    }
+}
--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -7,7 +7,7 @@ import lombok.*;
 @Getter
 @ToString(of = {"id", "userName", "age"})
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Member {
+public class Member extends BaseEntity {
     @Id
     @GeneratedValue
     @Column(name = "member_id")
